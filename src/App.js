59a1d31216b3031bb17480bf4a72c5ea09a3e6ff
pablo kohn <pablokohn@gmail.com>
2023-04-17 07:44:40 -0300

    version con manejo de display de divs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [phVerb, setphVerb] = useState("phrasal verb");
+  const [def, setdef] = useState("definition");
+
+  // css
+  const [statePh, setStatePh] = useState({display:"none",color:"red"});
+  const [stateDef, setStateDef] = useState({display:"none"});
+
+  // dictionary
+  const phrasalVerbs = [{pverb:"Stand up", def:"Be on your feet"},{pverb:"Look after", def:"Care for someone"},
+  {pverb:"make up", def:"Create a new idea"},{pverb:"Pick on", def:"bother sb"}]
+
+  // functions
+  // new phrasal verb button
+  const chooseNewPHVerb = () => {
+    const element = phrasalVerbs[Math.floor(Math.random() * phrasalVerbs.length)];
+    
+    setphVerb(element.pverb);
+    setdef(element.def);
+    
+    setStatePh({
+      display:"inline",
+      color:"red"
+    });
+    setStateDef({display:"none"});    
+  } 
+
+  // show answer button
+  const showAnswer = () => {
+    setStateDef({display:"inline"});
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Brush up
-        </p>
-      </header>
+        <h1>Brush up</h1>
+        <button onClick={chooseNewPHVerb}>New phrasal verb</button>
+        <p style={statePh}>{phVerb}:</p>
+        <button onClick={showAnswer}>show answer</button>
+        <p style={stateDef}>{def}</p>
+      </header>  
     </div>
   );
 }
