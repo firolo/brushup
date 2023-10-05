e6c44a4b81f6e5c08d1cc519a96eb2be41015a3b
Pablo Kohn <pablokohn@gmail.com>
2023-10-05 07:38:55 -0300

    mock for some words

--- src/components/LookUpForm.js
+++ src/components/LookUpForm.js
@@ -14,12 +14,29 @@ export function LookUpForm() {
             setDefinition(data[0])})
         }
 
+    function lookupTest(wordToLookUp) {
+        fetch(`/entries/en/test`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            setDefinition(data[0])})
+        }
+
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            // 👇 Get input value
+            lookupTest(word)
+        }
+    }
+
     return(
         <>
             <input id="lookup" placeholder="Search for a word"
                 onChange={e => {
                     return setWord(e.target.value);
                 }}
+                onKeyDown={handleKeyDown}
             />
             <button onClick={()=>{lookup(word)}}>Search</button>
             <Definition def={definition}></Definition>
--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const { worker } = require('./mocks/browser')
-worker.start()
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser')
+  worker.start()
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
