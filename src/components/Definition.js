6d0339ab70c2b632f4939a23115acc8a4f71b23b
Pablo Kohn <pablokohn@gmail.com>
2023-10-19 17:20:21 -0300

    definion's array is displayed after search

--- src/components/Definition.js
+++ src/components/Definition.js
@@ -1,10 +1,13 @@
-
+import './css/Definition.css';
 
 export function Definition(props) {
 
     return (
-        <>
-            <div>{props.def.word} {props.def.phonetic}</div>
-        </>
+            <>
+                <div className="definition">{props.def.word} {props.def.phonetic}</div>
+                <div className="definition">Definitions:</div>
+                {props.def.meanings.map(e => e.definitions.map(f=> {
+                    return (<div className="definition">{f.definition}: {f.example}</div>)}))}
+            </>
     )
 }
